ffa3834b955aac89680b72a5350b4e829bb00edc
aayushgenius <69508057+aayushgenius@users.noreply.github.com>
2020-10-10 20:36:24 +0530

    Create aboutus.C

---
+++ About us/aboutus.C
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+  puts("About AlphaQuad");
+  puts("AlphaQuad us a project to make new chatbots for free. Check out the github repo at https://github.com/aayushgenius/AlphaQuad");
+  return(0);
+}
