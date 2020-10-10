4ded84a90999ac4865dd5d0915a58fe2db48e199
aayushgenius <69508057+aayushgenius@users.noreply.github.com>
2020-10-10 20:36:39 +0530

    Delete aboutus.C

--- aboutus.C
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  puts("About AlphaQuad");
-  puts("AlphaQuad us a project to make new chatbots for free. Check out the github repo at https://github.com/aayushgenius/AlphaQuad");
-  return(0);
-}
